3195356795b0a88a72312f79dd694187f3b67f3b
Aygün Bayır <turkoaygun@gmail.com>
2024-12-09 16:27:47 +0300

    Mediatr Pipeline ile Validation İmplementasyonu

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -22,10 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-if(app.Environment.IsProduction())
-{
+//if(app.Environment.IsProduction())
+//{
     app.ConfigureCustomExceptionMiddleware();
-}
+//}
 
 app.UseHttpsRedirection();
 
