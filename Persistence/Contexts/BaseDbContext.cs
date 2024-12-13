82388909c45547757b60dee556b63920b195c177
Aygün Bayır <turkoaygun@gmail.com>
2024-12-14 02:38:44 +0300

    End Series 4

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
@@ -11,10 +12,16 @@ public class BaseDbContext : DbContext
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Car> Cars { get; set; }
     public DbSet<Fuel> Fuels { get; set; }
-    public DbSet<Model> Models { get; set; }
     public DbSet<Transmission> Transmissions { get; set; }
+    public DbSet<Model> Models { get; set; }
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
 
-    public BaseDbContext(DbContextOptions dbContextOptions,IConfiguration configuration) : base(dbContextOptions)
+    public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
     {
         Configuration = configuration;
         Database.EnsureCreated();
@@ -23,6 +30,5 @@ public class BaseDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        base.OnModelCreating(modelBuilder);
     }
 }
